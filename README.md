d6051d541363b6b8ad6a5dd98a36d7050eccf74e
grockidile <grockidile@localhost.localdomain>
2019-02-10 18:46:45 -0800

    set up to provide system input

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# HP-Docker-Capstone-Input-Manager
-Allows us to specify the inputs for the system to work on
+# HP-Docker-Capstone
+
+Start Docker Beanstalk with the following command
+docker run -d -p 11300:11300 schickling/beanstalkd
+
+Client Library for beanstalkd Java
+https://github.com/jpeffer/JavaBeanstalkClient
+
+Build with 
+./gradlew build
+
+Built file found in /build/libs
+run the *-all* file with
+java -jar <filename>
+
+Make sure the beanstalk connection in .groovy is connected to the same port the Docker container is running on
+
+
+Make sure the beanstalkd container and our contaner our running on the same network (use the --net=host)
+example run command:
+
+docker run --net=host work-manager
---
+++ src/main/groovy/com/hp/pwp/capstone/BeanstalkClient.groovy
@@ -0,0 +1,61 @@
+package com.hp.pwp.capstone
+
+import com.surftools.BeanstalkClientImpl.ClientImpl
+import com.surftools.BeanstalkClientImpl.JobImpl
+import com.surftools.BeanstalkClient.BeanstalkException
+
+
+class BeanstalkClient{
+	private ClientImpl connection = new ClientImpl("0.0.0.0", 11300);
+	private JobImpl currentJob;	//can we only be working on one job at a time?
+	public List<String> listTubes(){
+		connection.useTube("riak")
+			return connection.listTubes();
+	}
+
+	public void sendWork(String json){
+		long priority = 0;
+		int delaySeconds = 0;
+		int timeToRun = 10;
+		byte[] data = json.getBytes();
+
+		connection.put(priority, delaySeconds, timeToRun, data);
+	}	
+	//functionhere for testing purposes
+	public void useTube(String s){
+		connection.useTube(s);
+	}
+	//pull a new job off the new_work queue
+	public String recieve_new_work(){
+		connection.watch("new_work");
+		JobImpl job = connection.reserve();
+		String s = new String(job.data);
+		connection.delete(job.jobId);
+		return s;
+	}
+	//put a new job on the to_workerB queue
+	public void send_to_workerB(String json){
+		connection.useTube("to_worker_b");
+		sendWork(json);
+		
+		
+	}
+	//pull a job off of the riak queue
+	//returns a string of json data and the job is deleted
+	public String recieve_riak_work(){
+		connection.watch("riak");
+		JobImpl job = connection.reserve();
+		String s = new String(job.data);
+		connection.delete(job.jobId);
+		return s;
+
+	}
+	//put a job on the status queue
+	public void send_status(String json){
+		connection.usetTube("status");
+		sendWork(json);
+	}
+
+
+
+}
